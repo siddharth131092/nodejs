01df2fb263d92d6606caf220d4885df1e227d88a
siddharth131092 <siddbramahne1310@gmail.com>
2024-09-05 05:41:34 +0530

    all

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('good, World!');
+  res.send('good, all!');
 });
  
 // Define the port number and start the server
